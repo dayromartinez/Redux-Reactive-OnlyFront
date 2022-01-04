3ce6c98073565fc1c37d63f526b0940c4097f74a
Dayro Martínez <djmd96@hotmail.com>
2022-01-03 20:54:10 -0500

    Configuracion deploy firebase front

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,9 @@
 export default {
-  apiKey: "AIzaSyAnSSYuK6_som3lPILWqg3jM3mQlGEeip4",
-  authDomain: "reactdemoproject-90995.firebaseapp.com",
-  projectId: "reactdemoproject-90995",
-  storageBucket: "reactdemoproject-90995.appspot.com",
-  messagingSenderId: "807877848827",
-  appId: "1:807877848827:web:277b0324a281c5c1568a5e",
-  measurementId: "G-GGGWR752Z5"
+  apiKey: "AIzaSyC20VpW54nSNwca50vM84R1vE_Bb2owfa8",
+  authDomain: "app-citas-front.firebaseapp.com",
+  projectId: "app-citas-front",
+  storageBucket: "app-citas-front.appspot.com",
+  messagingSenderId: "889660572603",
+  appId: "1:889660572603:web:e283b8b153b91d7323ba9e",
+  measurementId: "G-20H6QJGN2S"
 };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider, connect } from 'react-redux';
-import store from './store/index'
 import firebaseConfig from './firebase-config';
 import {
   FirebaseAppProvider
-} from 'reactfire'
-import Form from './components/Form';
+} from 'reactfire';
 
 ReactDOM.render((
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-    <React.StrictMode>
       <Suspense fallback={"Conectando a la app..."}>
         <App />
       </Suspense>
-    </React.StrictMode>
   </FirebaseAppProvider>
-),
-  document.getElementById('root')
+), document.getElementById('root')
 );
